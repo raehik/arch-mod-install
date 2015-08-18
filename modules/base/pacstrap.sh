78ea3811a183bc5e38593089a1a9520e15396235
Ben Orchard <thefirstmuffinman@gmail.com>
2015-08-18 15:36:13 +0100

    modules/pacstrap: update keyring first

--- modules/base/pacstrap.sh
+++ modules/base/pacstrap.sh
@@ -4,9 +4,12 @@
 # Desc.:    Install the base system.
 #
 
+_log "Updating keyring"
+pacman -Sy archlinux-keyring
+
 _log "Ranking pacman mirrors (may take 5 minutes or more)"
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
-rankmirrors -n 6 /etc/pacman.d/mirrorlist.bak > /etc/pacman.d/mirrorlist
+rankmirrors -n 10 /etc/pacman.d/mirrorlist.bak > /etc/pacman.d/mirrorlist
 
 _log "Pacstrapping base packages"
 pacstrap -i /mnt base grub openssh rxvt-unicode-terminfo sudo
