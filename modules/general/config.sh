5265939659e0682613ef48528d3590ba9f421ae5
Ben Orchard <thefirstmuffinman@gmail.com>
2015-08-18 16:00:30 +0100

    general/config: move comments -> logs

--- modules/general/config.sh
+++ modules/general/config.sh
@@ -4,34 +4,35 @@
 # Desc.:    General system configuration (hostname, locale etc.)
 #
 
-# set hostname
+_log "Setting hostname: $V_hostname"
 echo "$V_hostname" > /etc/hostname
 
-# set time zone
+_log "Setting timezone: $V_timezone"
 ln -sf "/usr/share/zoneinfo/$V_timezone" /etc/localtime
 
-# generate locales
+_log "Generating some locales"
 sed -i \
     -e 's/^#en_GB.UTF-8/en_GB.UTF-8/' \
     -e 's/^#en_US.UTF-8/en_US.UTF-8/' \
     /etc/locale.gen
 locale-gen
 
-# set preferred locale & keymap
+_log "Setting preferred locale & keymap"
 localectl set-locale "LANG=$V_locale"
 localectl set-keymap "$V_keymap"
 
 ## LVM-related options {{{
 if [[ "$V_M_disk" == *"lvm"* ]]; then
-    # add lvm2 hook for mkinitcpio
+    _log "LVM install detected, adding lvm2 hook for mkinitcpio"
     sed -i \
         -e 's/\(^HOOKS=".* block\) \(filesystems .*"\)/\1 lvm2 \2/' \
         /etc/mkinitcpio.conf
 fi
 ## }}}
 
-# create ramdisk
+_log "Creating ramdisk with mkinitcpio"
 mkinitcpio -p linux
 
 # upgrade pacman db (depending on install media age, this might be necessary)
+_log "Running pacman-db-upgrade"
 pacman-db-upgrade
