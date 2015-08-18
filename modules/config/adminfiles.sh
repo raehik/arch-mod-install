2d27e3163ed4ef5f02b61e735a7a382e98e2858f
Ben Orchard <thefirstmuffinman@gmail.com>
2015-08-18 16:35:39 +0100

    config/: install git first!

--- modules/config/adminfiles.sh
+++ modules/config/adminfiles.sh
@@ -4,6 +4,7 @@
 # Desc.:    Install my adminfiles.
 #
 
+_pkg_install git
 git clone "https://github.com/$V_github_user/adminfiles" "$V_chroot_tmp_dir"
 "$V_chroot_tmp_dir/adminfiles/install"
 rm -rf "$V_chroot_tmp_dir/adminfiles"
--- modules/config/grubfiles.sh
+++ modules/config/grubfiles.sh
@@ -1,9 +1,10 @@
 # vim: set ft=sh
 #
 # Module:   grubfiles
-# Desc.:    Install my grubfiles. 
+# Desc.:    Install my grubfiles.
 #
 
+_pkg_install git
 git clone "https://github.com/$V_github_user/grubfiles" "$V_chroot_tmp_dir"
 "$V_chroot_tmp_dir/grubfiles/install"
 rm -rf "$V_chroot_tmp_dir/grubfiles"
