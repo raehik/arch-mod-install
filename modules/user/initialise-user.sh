266e5771a175feb5262683fbc8d09afd87dc88ea
Ben Orchard <thefirstmuffinman@gmail.com>
2015-08-18 17:10:09 +0100

    new module: initialise user

---
+++ modules/user/initialise-user.sh
@@ -0,0 +1,9 @@
+# vim: set ft=sh
+#
+# Module:   user/initialise-user
+# Desc.:    Initialise a user.
+#
+
+_log "Initialising new user $V_user"
+useradd -m "$V_user"
+chpasswd <<< "$V_user:$V_user_passwd"
