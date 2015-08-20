516644c719059d9283b8a6b75748a9b3ccd6e358
Ben Orchard <thefirstmuffinman@gmail.com>
2015-08-20 13:56:29 +0100

    net/avahi: enable using .local domains

--- modules/net/avahi.sh
+++ modules/net/avahi.sh
@@ -5,5 +5,8 @@
 #
 
 _log "Installing and enabling Avahi"
-_pkg_install avahi
+_pkg_install avahi nss-mdns
+sed -e 's/\(^hosts: files dns myhostname\)/#\1/' \
+    -e '10ihosts: files mdns_minimal [NOTFOUND=return] dns myhostname' \\
+    -i /etc/nsswitch.conf
 systemctl enable avahi-daemon
