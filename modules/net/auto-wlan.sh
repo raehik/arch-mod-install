a7a13482685e295f91136608267324c5fde49398
Ben Orchard <thefirstmuffinman@gmail.com>
2016-07-30 14:23:13 +0100

    merge net/wlan-setup into net/auto-wlan, fix it

--- modules/net/auto-wlan.sh
+++ modules/net/auto-wlan.sh
@@ -4,5 +4,9 @@
 # Desc.:    Auto-connect to wireless networks which have netctl profiles.
 #
 
-_pkg_install wpa_actiond
+_log "Installing dependencies for netctl-auto and wifi-menu"
+_pkg_install wpa_actiond dialog
+_log "Enabling netctl-auto on $V_wlan_interface"
 systemctl enable netctl-auto@${V_test}.service
+_log "Enabled, however no netctl profiles are present yet, so you'll have to
+create one on reboot"
--- modules/net/wlan-setup.sh
+++
@@ -1,8 +0,0 @@
-# vim: set ft=sh
-#
-# Module:   net/wlan-setup
-# Desc.:    Set up a wireless network netctl profile using wifi-menu.
-#
-
-_pkg_install wifi-menu
-wifi-menu
