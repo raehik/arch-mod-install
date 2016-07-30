2fdf27a4a45619942a0a570ca761043a94efca39
Ben Orchard <thefirstmuffinman@gmail.com>
2016-07-30 14:24:17 +0100

    net/auto-wlan: fix interface var & add in vars

--- modules/net/auto-wlan.sh
+++ modules/net/auto-wlan.sh
@@ -7,6 +7,6 @@
 _log "Installing dependencies for netctl-auto and wifi-menu"
 _pkg_install wpa_actiond dialog
 _log "Enabling netctl-auto on $V_wlan_interface"
-systemctl enable netctl-auto@${V_test}.service
+systemctl enable netctl-auto@${V_wlan_interface}.service
 _log "Enabled, however no netctl profiles are present yet, so you'll have to
 create one on reboot"
